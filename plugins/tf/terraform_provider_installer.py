13ee9581f9c70df3bfc1d19aeb134c7b669b04bc
Guillaume Everarts de Velp <guillaume.everartsdevelp@inmanta.com>
2022-10-24 14:42:07 +0200

    Add timeout on http requests

--- plugins/tf/terraform_provider_installer.py
+++ plugins/tf/terraform_provider_installer.py
@@ -83,7 +83,7 @@ class ProviderInstaller:
             arch = "amd64"
 
         # Get provider available versions
-        response = requests.get(f"{BASE_URL}/{self.namespace}/{self.type}")
+        response = requests.get(f"{BASE_URL}/{self.namespace}/{self.type}", timeout=3)
         response.raise_for_status()
         data = response.json()
         versions = data.get("versions")
@@ -96,7 +96,8 @@ class ProviderInstaller:
 
         # Get provider specific version info
         response = requests.get(
-            f"{BASE_URL}/{self.namespace}/{self.type}/{self.version}/download/{system}/{arch}"
+            f"{BASE_URL}/{self.namespace}/{self.type}/{self.version}/download/{system}/{arch}",
+            timeout=3,
         )
         response.raise_for_status()
         data = response.json()
@@ -145,7 +146,7 @@ class ProviderInstaller:
             raise InstallerException("The provided download path is a directory")
 
         # Download the file and stream the output to a file, while computing the shasum of it
-        with requests.get(self.download_url, stream=True) as r:
+        with requests.get(self.download_url, stream=True, timeout=3) as r:
             r.raise_for_status()
             sha256_hash = hashlib.sha256()
             with open(str(download_location), "wb") as f:
